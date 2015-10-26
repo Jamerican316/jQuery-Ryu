b811f0de8753ebfb5dea3c68cde7b29d60b64442
Jamerican316 <karanja.broomfield@gmail.com>
2015-10-26 12:48:01 -0700

    RyuStill and RyuReady mouse events added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  
+	
+  $('.ryu').mouseenter(function() {
+  	/* used alert to test code for functionality
+  	alert('mouse entered .ryu div');*/
+  	$('.RyuStill').hide();
+  	$('.RyuReady').show();
+	})
+  .mouseleave(function() {
+    $('.RyuReady').hide();
+	$('.RyuStill').show();
+  });
 });
 $(".RyuStill").hover( handler ).show
   	$(".RyuStill").hide(".RyuReady");
